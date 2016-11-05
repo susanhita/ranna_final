481ce15982494cade66a6a27764c625ad7cb18ba
susanhita <susanhita@gmail.com>
2016-11-05 12:05:51 +0530

    this was soo funny.I had to save it

--- app/src/main/java/com/habijabi/ranna/SplashScreen.java
+++ app/src/main/java/com/habijabi/ranna/SplashScreen.java
@@ -13,18 +13,27 @@ public class SplashScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        overridePendingTransition(R.anim.flip,R.anim.flip);
+
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.splash);
 
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(2000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
                     Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
 
 
-        Activity activity = this;
-        //activity.overridePendingTransition(R.anim.flip,R.anim.move);
-      //  activity.finish();
-        activity.overridePendingTransition(R.anim.flip,R.anim.move);
-        activity.startActivity(intent);
     }
 
     @Override
@@ -33,6 +42,41 @@ public class SplashScreen extends Activity {
         finish();
     }
 
+    @Override public void finish()
+    {
+        super.finish();
+        overridePendingTransition(R.anim.flip, R.anim.flip);
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
